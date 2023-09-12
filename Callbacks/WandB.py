f9748fe22e72d2d9ce1eb18fdab981c1b788534f
HoangNam <phn1712002@gmail.com>
2023-09-12 08:52:49 +0700

    Cập nhật show thông tin train trên wandb

--- Callbacks/WandB.py
+++ Callbacks/WandB.py
@@ -1,4 +1,6 @@
 import wandb
+import tensorflow as tf
+import numpy as np
 from keras.callbacks import Callback
 from Tools.Weights import getPathWeightsNearest
 from Architecture.Pipeline import PriceRecognize_VGG16
@@ -12,6 +14,24 @@ class CustomCallbacksWandB(Callback):
         self.__last_name_update = None
         
     def on_epoch_end(self, epoch: int, logs=None):
+        
+        # Sao lưu một mẫu âm thanh kiểm tra
+        tableOutputPredict = wandb.Table(columns=["Epoch", "Input", "Output"])
+        for X, _ in self.dev_dataset.take(1):
+            if not X.shape[0] == 1:
+                index = np.random.randint(low=0, high=X.shape[0] - 1)
+                X = X[index]
+                X = tf.expand_dims(X, axis=0)
+        
+        Y = self.model.predict_on_batch(X)
+        output = self.pipeline.decoderLable(Y)
+        
+        image_output_wandb = wandb.Image(Y.numpy())
+            
+        tableOutputPredict.add_data(epoch + 1, image_output_wandb, output)
+        wandb.log({'Predict': tableOutputPredict}) 
+        
+        
         # Cập nhật file weights model to cloud wandb
         path_file_update = getPathWeightsNearest(self.path_logs)
         if self.__last_name_update != path_file_update: 
