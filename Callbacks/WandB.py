de296010795637fd672ffdfbdb963687c623a0fd
HoangNam <phn1712002@gmail.com>
2023-09-13 09:30:02 +0700

    Cập nhật save file h5

--- Callbacks/WandB.py
+++ Callbacks/WandB.py
@@ -15,27 +15,28 @@ class CustomCallbacksWandB(Callback):
         
     def on_epoch_end(self, epoch: int, logs=None):
         
-        # Sao lưu một mẫu âm thanh kiểm tra
-        tableOutputPredict = wandb.Table(columns=["Epoch", "Input", "Output"])
-        for X, _ in self.dev_dataset.take(1):
-            if not X.shape[0] == 1:
-                index = np.random.randint(low=0, high=X.shape[0] - 1)
-                X = X[index]
-                X = tf.expand_dims(X, axis=0)
-        
-        Y = self.model.predict_on_batch(X)
-        output = self.pipeline.decoderLable(Y)
-        
-        image_input_wandb = wandb.Image(tf.squeeze(X).numpy())
+        if epoch % 10 == 0:
+            # Sao lưu một mẫu âm thanh kiểm tra
+            tableOutputPredict = wandb.Table(columns=["Epoch", "Input", "Output"])
+            for X, _ in self.dev_dataset.take(1):
+                if not X.shape[0] == 1:
+                    index = np.random.randint(low=0, high=X.shape[0] - 1)
+                    X = X[index]
+                    X = tf.expand_dims(X, axis=0)
             
-        tableOutputPredict.add_data(epoch + 1, image_input_wandb, output)
-        wandb.log({'Predict': tableOutputPredict}) 
+            Y = self.model.predict_on_batch(X)
+            output = self.pipeline.decoderLable(Y)
+            
+            image_input_wandb = wandb.Image(tf.squeeze(X).numpy())
+                
+            tableOutputPredict.add_data(epoch + 1, image_input_wandb, output)
+            wandb.log({'Predict': tableOutputPredict}) 
         
         
         # Cập nhật file weights model to cloud wandb
         path_file_update = getPathWeightsNearest(self.path_logs)
         if self.__last_name_update != path_file_update: 
-            if not self.__last_name_update is None: os.remove(self.__last_name_update)
+            #if not self.__last_name_update is None: os.remove(self.__last_name_update)
             self.__last_name_update = path_file_update
             wandb.save(path_file_update)
         
--- Tools/Callbacks.py
+++ Tools/Callbacks.py
@@ -6,7 +6,7 @@ from wandb.keras import WandbCallback
 def createCallbacks(PATH_TENSORBOARD, PATH_LOGS, config, train_dataset, dev_dataset, pipeline):
     NAME_TIME = time.strftime("%Y%m%d-%H%M%S-")
     tensorBoard_callbacks = TensorBoard(log_dir=PATH_TENSORBOARD)
-    checkpoint_callbacks = ModelCheckpoint(filepath=PATH_LOGS + NAME_TIME + 'weights-{epoch:03d}.h5', save_weights_only=True, **config['config_train']['checkpoint'])
+    checkpoint_callbacks = ModelCheckpoint(filepath=PATH_LOGS + NAME_TIME + 'weights-{epoch:03d}.h5', save_best_only=True, save_weights_only=True, **config['config_train']['checkpoint'])
     callbacks_model = [tensorBoard_callbacks, checkpoint_callbacks]
     if config['config_wandb']['using'] == True:
         os.environ['WANDB_API_KEY'] = config['config_wandb']['api_key']
