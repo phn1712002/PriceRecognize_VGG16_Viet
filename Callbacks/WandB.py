ffa9225d50e66a279fdb43e5b398355aab37b862
HoangNam <phn1712002@gmail.com>
2023-09-19 19:16:22 +0700

    Cập nhật callbacks

--- Callbacks/WandB.py
+++ Callbacks/WandB.py
@@ -1,21 +1,54 @@
-import wandb, os
+import wandb, os 
 import tensorflow as tf
 import numpy as np
 from keras.callbacks import Callback
-from Tools.Weights import getPathWeightsNearest
 from Architecture.Pipeline import PriceRecognize_VGG16
 
 class CustomCallbacksWandB(Callback):
-    def __init__(self, pipeline: PriceRecognize_VGG16, path_logs='./Checkpoint/logs/', dataset=None):
+    def __init__(self, pipeline: PriceRecognize_VGG16,
+                 name_save='model.h5',
+                 monitor='val_loss',
+                 verbose=1,
+                 save_freq=1, 
+                 mode='min',
+                 path_logs='./Checkpoint/logs/', 
+                 dataset=None):
+        
         super().__init__()
         self.dataset = dataset
         self.path_logs = path_logs
         self.pipeline = pipeline
         
+        self.name_save = name_save
+        self.history_checkpoint = []
+        self.monitor = monitor
+        self.save_freq = save_freq
+        self.mode = mode
+        self.verbose = verbose
+        
     def on_epoch_end(self, epoch: int, logs=None):
+        
         # Sao lưu logs wandb
         wandb.log(logs)
         
+        # Checkpoint
+        self.history_checkpoint.append(logs[self.monitor])
+        save_model_checkpoint = False
+        if self.mode == 'min':
+            save_model_checkpoint = min(self.history_checkpoint) > logs[self.monitor]
+        elif self.mode == 'max':
+            save_model_checkpoint = max(self.history_checkpoint) < logs[self.monitor]
+            
+        # Sao lưu mô hình 
+        if self.save_freq > 0 and epoch % self.save_freq == 0 and save_model_checkpoint:
+            path = self.path_logs + self.name_save
+            self.model.save_weights(path)
+            wandb.save(path)
+            os.remove(path)
+            if self.verbose == 1:
+                print(f"Save weights epoch {epoch+1} - {path}" )
+           
+            
         # Sao lưu một mẫu âm thanh kiểm tra
         tableOutputPredict = wandb.Table(columns=["Epoch", "Input", "Output Target", "Output Pred"])
         for X, Y in self.dataset.take(1):
@@ -36,6 +69,4 @@ class CustomCallbacksWandB(Callback):
         wandb.log({'Predict': tableOutputPredict}) 
         
         
-        # Sao lưu h5
-        path_file_update = getPathWeightsNearest(self.path_logs)
-        wandb.save(path_file_update)
+       
--- train.py
+++ train.py
@@ -1,14 +1,16 @@
 
-import tensorflow as tf, warnings, wandb, argparse
+import tensorflow as tf, warnings, wandb, argparse, time, os
 from Architecture.Model import VGG16
 from Dataset.CreateDataset import PriceRecognize_Dataset_Vietnamese
 from Architecture.Pipeline import PriceRecognize_VGG16
 from Optimizers.OptimizersVGG16 import CustomOptimizers
+from Callbacks.WandB import CustomCallbacksWandB
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from Tools.Json import loadJson
+from Tools.Folder import createFolder
 from jlclient import jarvisclient
 from jlclient.jarvisclient import *
-from Tools.Json import loadJson
-from Tools.Callbacks import createCallbacks
-from Tools.Folder import createFolder
+
 
 
 # Environment Variables
@@ -64,6 +66,7 @@ if config_dataset['using_augments']: config_augments = config_dataset['config_au
 # Create pipeline 
 pipeline = PriceRecognize_VGG16(class_names=class_names, config_model=config_model)
 
+# Create dataset
 train_dataset = PriceRecognize_VGG16(class_names=class_names, 
                                      config_augments=config_augments, 
                                      config_model=config_model)(dataset=train_dataset_raw, batch_size=config_dataset['batch_size_train'])
@@ -78,13 +81,33 @@ test_dataset = PriceRecognize_VGG16(class_names=class_names,
 opt_VGG16 = CustomOptimizers(**config_opt)()
 
 # Callbacks
-callbacks_VGG16= createCallbacks(PATH_TENSORBOARD=PATH_TENSORBOARD, 
-                                PATH_LOGS=PATH_LOGS, 
-                                config=config, 
-                                train_dataset=train_dataset,
-                                test_dataset=test_dataset,
-                                dev_dataset=dev_dataset, 
-                                pipeline=pipeline)
+NAME_TIME = time.strftime("%Y%m%d-%H%M%S-")
+tensorBoard_callbacks = TensorBoard(log_dir=PATH_TENSORBOARD)
+callbacks = [tensorBoard_callbacks]
+
+if config['config_wandb']['using'] == True:
+    os.environ['WANDB_API_KEY'] = config['config_wandb']['api_key']
+    wandb.login()
+    wandb.tensorboard.patch(root_logdir=PATH_TENSORBOARD)
+    config_update = config.copy()
+    config_update.pop('config_wandb')
+    config_update.pop('config_jarvislabs')
+    wandb.init(project=config['config_wandb']['project'],
+            name=NAME_TIME + config['config_wandb']['name'],
+            sync_tensorboard=config['config_wandb']['sync_tensorboard'],
+            config=config_update)
+    custom_WandB = CustomCallbacksWandB(name_save=NAME_TIME + 'epoch-{epoch:02d}.h5',
+                                        path_logs=PATH_LOGS, 
+                                        pipeline=pipeline, 
+                                        dataset=test_dataset
+                                        **config['config_train']['checkpoint'])
+    callbacks.append(custom_WandB)
+    
+else:
+    checkpoint_callbacks = ModelCheckpoint(filepath=PATH_LOGS + NAME_TIME + 'epoch-{epoch:02d}.h5', 
+                                           save_weights_only=True, 
+                                           **config['config_train']['checkpoint'])
+    callbacks.append(checkpoint_callbacks)
 
 # Create model
 model = VGG16(class_names=class_names, 
@@ -97,11 +120,11 @@ if args.pretrain_config:
         model = model.loadWeights(path=PATH_LOGS)
     else: 
         model = model.loadWeights(path=PATH_LOGS, name_file=args.name_file_pretrain)
-        
+     
 # Train model
 model.fit(train_dataset=train_dataset, 
           dev_dataset=dev_dataset, 
-          callbacks=callbacks_VGG16,
+          callbacks=callbacks,
           epochs=config_train['epochs'])
 
 # Export to tflite
