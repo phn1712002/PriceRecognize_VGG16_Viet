fca65d8b70dcdc1b88bf3e10b032565d62036e0b
HoangNam <phn1712002@gmail.com>
2023-11-05 18:53:53 +0700

    Cập nhật check_stop

--- ControlServer/checking_stop.py
+++ ControlServer/checking_stop.py
@@ -8,14 +8,14 @@ from jlclient.jarvisclient import *
 
 # Environment Variables
 PATH_CONFIG = './config.json'
-sleep_duration = 60 * 5
+PATH_CONFIG_CHECK_STOP = './config_checking_stop.json'
 
 # Execute 'ps -ef' command on a Linux terminal
 processes = subprocess.run(['ps', '-ef'], stdout=subprocess.PIPE, text=True)
 print(processes.stdout)  # Display the list of running processes
 
-
 # Get configuration details
+config_check_stop = loadJson(PATH_CONFIG_CHECK_STOP)
 config = loadJson(PATH_CONFIG)
 if not config == None:
     # Check for required keys in the configuration file
@@ -29,13 +29,9 @@ if not config == None:
 print("-"*100)
 pid_to_check = input("Enter PID: ")
 print("-"*100)
-# Input info gmail
-sender_email = input("Enter gmail sender: ")
-sender_password = input("Enter gmail sender pass: ")
-receiver_email = input("Enter gmail receiver: ")
-print("-"*100)
 
 
+# Checking
 try:
     pid_to_check = int(pid_to_check)
 except ValueError:
@@ -49,8 +45,11 @@ else:
             time_now = time.strftime("%Y%m%d-%H%M%S")
             str_print = f"PID {pid_to_check} is running at time {time_now}"
             print(str_print)
-            sendEmail(sender_email, sender_password, receiver_email, 'ServerAI', str_print)
-            time.sleep(sleep_duration)
+            sendEmail(config_check_stop['sender_email'], 
+                      config_check_stop['sender_password'], 
+                      config_check_stop['receiver_email'], 'ServerAI', 
+                      str_print)
+            time.sleep(config_check_stop['sleep_duration'])
         else:
             # If the provided PID is not running, pause the Jarvis instance
             jarvisclient.token = config_jarvislabs['token']
