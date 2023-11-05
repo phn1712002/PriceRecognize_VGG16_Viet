4df032c5d12786afce9338ffdc6682aea1254d67
HoangNam <phn1712002@gmail.com>
2023-11-05 11:29:07 +0700

    Cập nhật checking_stop

--- ControlServer/checking_stop.py
+++ ControlServer/checking_stop.py
@@ -1,3 +1,4 @@
+import subprocess
 import psutil
 import time
 from Tools.Json import loadJson
@@ -8,33 +9,41 @@ from jlclient.jarvisclient import *
 PATH_CONFIG = './config.json'
 sleep_duration = 60
 
-# Get config
+# Execute 'ps -ef' command on a Linux terminal
+processes = subprocess.run(['ps', '-ef'], stdout=subprocess.PIPE, text=True)
+print(processes.stdout)  # Display the list of running processes
+
+# Get configuration details
 config = loadJson(PATH_CONFIG)
 if not config == None:
+    # Check for required keys in the configuration file
     keys_to_check = ['config_jarvislabs']
     if all(key in config for key in keys_to_check):
         config_jarvislabs = config['config_jarvislabs']
     else:
-        raise RuntimeError("Error config")
+        raise RuntimeError("Error in configuration file")
 
-# Nhập PID cần kiểm tra
-pid_to_check = input("Nhập PID: ")
+# Enter the PID to check
+print("-"*100)
+pid_to_check = input("Enter PID: ")
+print("-"*100)
 
 try:
     pid_to_check = int(pid_to_check)
 except ValueError:
-    print("PID phải là một số nguyên.")
+    print("PID must be an integer.")
 else:
+    # Continuously monitor the entered PID
     while True:
         if psutil.pid_exists(pid_to_check):
+            # Retrieve information about the process with the provided PID
             process = psutil.Process(pid_to_check)
             time_now = time.strftime("%Y%m%d-%H%M%S")
-            print(f"PID {pid_to_check} đang chạy tại thời gian {time_now}")
+            print(f"PID {pid_to_check} is running at time {time_now}")
             time.sleep(sleep_duration)
         else:
+            # If the provided PID is not running, pause the Jarvis instance
             jarvisclient.token = config_jarvislabs['token']
             jarvisclient.user_id = config_jarvislabs['user_id']
             instance = User.get_instances()[0]
-            instance.pause()
-
-        
+            instance.pause()  # Pause Jarvis instance
